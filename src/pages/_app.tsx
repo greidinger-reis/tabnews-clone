765aba2fbfac528dfaf16ca09fcf604bb0323b36
greidinger.dev <jp.greidinger@hotmail.com>
2022-11-04 23:24:33 -0300

    header, login form

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,122 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-
+import { SessionProvider, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
-
 import "../styles/globals.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import classNames from "classnames";
+import Dropdown from "../components/headlessui/Dropdown";
+import { FiChevronDown } from "react-icons/fi";
+import { IoNewspaperOutline } from "react-icons/io5";
+
+export const HEADER_HEIGHT = "64px";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+    Component,
+    pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+    return (
+        <SessionProvider session={session}>
+            <Navbar />
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 };
+
+function Navbar() {
+    const router = useRouter();
+    const { data: session } = useSession();
+
+    return (
+        <header
+            style={{ height: HEADER_HEIGHT }}
+            className="flex items-center justify-between bg-zinc-800 px-4 text-white"
+        >
+            <ul className="flex items-center gap-4 text-sm font-medium">
+                <li>
+                    <Link
+                        className="flex items-center gap-1 transition-all hover:text-zinc-400"
+                        href="/"
+                    >
+                        <IoNewspaperOutline className="text-2xl" />
+                        TabNews Clone
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className={classNames(
+                            "transition-all hover:border-zinc-400 hover:text-zinc-400",
+                            {
+                                "border-b border-white pb-1":
+                                    router.asPath === "/",
+                            }
+                        )}
+                        href="/"
+                    >
+                        Relevantes
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className={classNames(
+                            "transition-all hover:border-zinc-400 hover:text-zinc-400",
+                            {
+                                "border-b border-white pb-1":
+                                    router.asPath === "/recentes",
+                            }
+                        )}
+                        href="/recentes"
+                    >
+                        Recentes
+                    </Link>
+                </li>
+            </ul>
+            <div>
+                {session ? (
+                    <div>
+                        <span>Olá, {session.user?.name}</span>
+                        <Dropdown
+                            button={
+                                <button>
+                                    <FiChevronDown />
+                                </button>
+                            }
+                        >
+                            <ul>
+                                <li>
+                                    <Link href="/perfil/posts">Meus posts</Link>
+                                </li>
+                                <li>
+                                    <Link href="/perfil">Perfil</Link>
+                                </li>
+                                <li>
+                                    <button onClick={() => signOut()}>
+                                        Sair
+                                    </button>
+                                </li>
+                            </ul>
+                        </Dropdown>
+                    </div>
+                ) : (
+                    <div className="flex items-center gap-2 text-sm font-medium">
+                        <Link
+                            href="/login"
+                            className="transition-all hover:text-zinc-400"
+                        >
+                            Login
+                        </Link>
+                        <Link
+                            href="/cadastro"
+                            className="transition-all hover:text-zinc-400"
+                        >
+                            Cadastrar
+                        </Link>
+                    </div>
+                )}
+            </div>
+        </header>
+    );
+}
 
 export default trpc.withTRPC(MyApp);
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,74 @@
+import { HEADER_HEIGHT } from "../_app";
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import { FaDiscord } from "react-icons/fa";
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+    return (
+        <div
+            className="flex flex-col items-center justify-center"
+            style={{ height: `calc(100vh - ${HEADER_HEIGHT})` }}
+        >
+            <div className="my-2 mx-4 flex justify-center md:mx-0">
+                <form className="w-full max-w-xl rounded bg-white p-6 shadow-md">
+                    <h1 className="mb-4 text-center text-4xl font-bold tracking-tight text-zinc-700">
+                        Login
+                    </h1>
+                    <div className="-mx-3 mb-6 flex flex-wrap">
+                        <div className="mb-6 w-full px-3 md:w-full">
+                            <label
+                                className="ml-1 mb-1 block text-sm font-medium tracking-wide text-gray-700"
+                                htmlFor="Password"
+                            >
+                                Email
+                            </label>
+                            <input
+                                className="block w-full appearance-none rounded border border-gray-400 bg-white py-3 px-3 leading-tight text-gray-700 focus:outline-none"
+                                type="email"
+                                required
+                            />
+                        </div>
+                        <div className="mb-6 w-full px-3 md:w-full">
+                            <label
+                                className="ml-1 mb-1 block text-sm font-medium tracking-wide text-gray-700"
+                                htmlFor="Password"
+                            >
+                                Password
+                            </label>
+                            <input
+                                className="block w-full appearance-none rounded border border-gray-400 bg-white py-3 px-3 leading-tight text-gray-700 focus:outline-none"
+                                type="password"
+                                required
+                            />
+                        </div>
+                        <div className="w-full px-3 md:w-full">
+                            <button className="block w-full appearance-none rounded border border-gray-200 bg-green-600 py-3 px-3 font-semibold leading-tight text-gray-100 transition-all hover:bg-green-700 focus:border-gray-500 focus:bg-white focus:outline-none">
+                                Login
+                            </button>
+                        </div>
+                        <div className="relative mt-6 flex w-full items-center justify-center">
+                            <span className="z-10 bg-white px-4 text-xs text-gray-700">
+                                ou entre com
+                            </span>
+                            <div className="absolute h-[1px] w-11/12 bg-zinc-500" />
+                        </div>
+                        <div className="mt-4 flex w-full items-center justify-center gap-4">
+                            <button className="rounded border border-zinc-400 bg-gray-100 p-8 py-2 transition-all hover:bg-gray-200">
+                                <AiFillGithub className="text-2xl text-zinc-600" />
+                            </button>
+                            <button className="rounded border border-zinc-400 bg-gray-100 p-8 py-2 transition-all hover:bg-gray-200">
+                                <AiOutlineTwitter className="text-2xl text-zinc-600" />
+                            </button>
+                            <button
+                                onClick={() => signIn("discord")}
+                                className="rounded border border-zinc-400 bg-gray-100 p-8 py-2 transition-all hover:bg-gray-200"
+                            >
+                                <FaDiscord className="text-2xl text-zinc-600" />
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
