213343e4869f5c7a973b261ccacc1d5fa6c7cca5
greidinger.dev <jp.greidinger@hotmail.com>
2022-11-05 01:43:54 -0300

    cadastro page

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -1,11 +1,9 @@
-import { HEADER_HEIGHT } from "../_app";
+import { signIn } from "next-auth/react";
 import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
 import { FaDiscord } from "react-icons/fa";
-import { signIn } from "next-auth/react";
-import type { GetServerSideProps } from "next";
-import { getServerAuthSession } from "~/server/common/get-server-auth-session";
+import { HEADER_HEIGHT } from "../_app";
 
-export default function LoginPage() {
+export default function CadastroPage() {
     return (
         <div
             className="flex flex-col items-center justify-center"
@@ -14,38 +12,57 @@ export default function LoginPage() {
             <div className="my-2 mx-4 flex justify-center md:mx-0">
                 <form className="w-full max-w-xl rounded bg-white p-6 shadow-md">
                     <h1 className="mb-4 text-center text-4xl font-bold tracking-tight text-zinc-700">
-                        Login
+                        Cadastrar
                     </h1>
                     <div className="-mx-3 mb-6 flex flex-wrap">
                         <div className="mb-6 w-full px-3 md:w-full">
                             <label
                                 className="ml-1 mb-1 block text-sm font-medium tracking-wide text-gray-700"
-                                htmlFor="Password"
+                                htmlFor="name"
                             >
-                                Email
+                                Nome de usuário
                             </label>
                             <input
+                                id="name"
                                 className="block w-full appearance-none rounded border border-gray-400 bg-white py-3 px-3 leading-tight text-gray-700 focus:outline-none"
-                                type="email"
+                                type="text"
+                                placeholder="Seu nome/nickname"
                                 required
                             />
                         </div>
                         <div className="mb-6 w-full px-3 md:w-full">
                             <label
                                 className="ml-1 mb-1 block text-sm font-medium tracking-wide text-gray-700"
-                                htmlFor="Password"
+                                htmlFor="email"
+                            >
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                className="block w-full appearance-none rounded border border-gray-400 bg-white py-3 px-3 leading-tight text-gray-700 focus:outline-none"
+                                type="email"
+                                placeholder="Seu email"
+                                required
+                            />
+                        </div>
+                        <div className="mb-6 w-full px-3 md:w-full">
+                            <label
+                                className="ml-1 mb-1 block text-sm font-medium tracking-wide text-gray-700"
+                                htmlFor="password"
                             >
                                 Password
                             </label>
                             <input
+                                id="password"
                                 className="block w-full appearance-none rounded border border-gray-400 bg-white py-3 px-3 leading-tight text-gray-700 focus:outline-none"
                                 type="password"
+                                placeholder="Sua senha"
                                 required
                             />
                         </div>
                         <div className="w-full px-3 md:w-full">
                             <button className="block w-full appearance-none rounded border border-gray-200 bg-green-600 py-3 px-3 font-semibold leading-tight text-gray-100 transition-all hover:bg-green-700 focus:border-gray-500 focus:bg-white focus:outline-none">
-                                Login
+                                Criar cadastro
                             </button>
                         </div>
                         <div className="relative mt-6 flex w-full items-center justify-center">
@@ -81,20 +98,3 @@ export default function LoginPage() {
         </div>
     );
 }
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-    const session = await getServerAuthSession({ req, res });
-
-    if (session) {
-        return {
-            redirect: {
-                destination: "/",
-                permanent: false,
-            },
-        };
-    }
-
-    return {
-        props: {},
-    };
-};
