df380efefce8a82b0a6f4d470b0db1ebdd1035b9
greidinger.dev <jp.greidinger@hotmail.com>
2022-11-09 20:01:14 -0300

    unused import

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { CgSpinner } from "react-icons/cg";
 import { NextAuthProviderButtons } from "~/components/NextAuthProviderButtons";
-import Spinner from "~/components/Spinner";
 
 interface LoginFormData {
     email: string;
