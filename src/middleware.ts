c41f6182eed67c0d1ed06109620cbfd29e21636f
greidinger.dev <jp.greidinger@hotmail.com>
2022-11-09 02:22:39 -0300

    /login middleware 3rd attempt

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import { type NextRequest, NextResponse } from "next/server";
 
 // if there is a session, redirect to / (home)
 export function middleware(req: NextRequest) {
-    if (req.cookies.has("next-auth.session-token")) {
+    if (
+        req.cookies.has("next-auth.session-token") ||
+        req.cookies.has("__Secure-next-auth.session-token")
+    ) {
         console.log({
             status: "session exists",
             cookies: req.cookies,
