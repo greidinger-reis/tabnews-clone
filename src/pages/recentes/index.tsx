20df780716e63288a16634283952e22a678df957
greidinger.dev <jp.greidinger@hotmail.com>
2022-11-09 12:50:31 -0300

    recents also fixed

--- src/pages/recentes/index.tsx
+++ src/pages/recentes/index.tsx
@@ -1,7 +1,25 @@
+import type { GetStaticProps } from "next";
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { appRouter } from "~/server/trpc/router/_app";
+import { createContextInner } from "~/server/trpc/context";
+import superjson from "superjson";
 import { PostList } from "~/components/PostList";
 
-export default function RecentesPage() {
-    return (
-        <PostList recents={true}/>
-    );
+export const getStaticProps: GetStaticProps = async () => {
+    const ssg = createProxySSGHelpers({
+        router: appRouter,
+        ctx: await createContextInner({ session: null }),
+        transformer: superjson,
+    });
+
+    await ssg.posts.list.prefetchInfinite({ limit: 15, recents: true });
+
+    return {
+        props: { trpcState: ssg.dehydrate() },
+        revalidate: 10,
+    };
+};
+
+export default function HomePage() {
+    return <PostList recents />;
 }
