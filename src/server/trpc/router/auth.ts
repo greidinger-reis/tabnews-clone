41c99d2b09391cc9c7f3a5d2fd78cbe5d026c2f7
greidinger.dev <jp.greidinger@hotmail.com>
2022-11-10 14:01:34 -0300

    brought back send mail func to one file

--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -2,7 +2,62 @@ import {TRPCError} from "@trpc/server";
 import bcrypt from "bcrypt";
 import {z} from "zod";
 import {publicProcedure, router} from "./../trpc";
-import {sendConfirmationEmail} from "~/server/nodemailer";
+import Nodemailer from "nodemailer";
+import {env} from "~/env/server.mjs";
+import type {User} from "@prisma/client";
+
+const transporter = Nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: env.GMAIL_USER,
+        pass: env.GMAIL_PASSWORD,
+    },
+});
+
+async function sendConfirmationEmail(user: User): Promise<{ ok: boolean; error: Error | null }> {
+    let ok = false;
+    let error: Error | null = null;
+
+    const activationURL = `https://tabnews-clone.vercel.app/cadastro/ativar/${user.id}`
+
+    console.log({
+        status: "Sending confirmation email",
+    })
+
+    transporter.sendMail(
+        {
+            from: env.GMAIL_USER,
+            to: user.email,
+            subject: "TabNews Clone - Ative sua conta",
+            html: `<p>${user.username}, clique no link abaixo para ativar seu cadastro no TabNews(clone)</p>
+            <a href=${activationURL}>${activationURL}</a>
+            <p>Caso você não tenha feito esta requisição, ignore esse email.</p>
+            <br/>
+            <p>Atenciosamente,</p>
+            <p>João Paulo Greidinger dos Reis</p>
+            <a href="https://greidinger.dev">greidinger.dev</a>
+            `,
+        },
+        (err, info) => {
+            if (err) {
+                console.log(err);
+                error = err;
+            } else {
+                console.log(info);
+                ok = true;
+            }
+        }
+    );
+
+    console.log({
+        status: "Confirmation email sent",
+    })
+
+    return {
+        ok,
+        error,
+    }
+}
 
 export const authRouter = router({
     checkCredentials: publicProcedure
