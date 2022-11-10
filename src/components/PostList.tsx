d86097bf2d83861892c2b36dce5f07ae02cf2fa5
greidinger.dev <jp.greidinger@hotmail.com>
2022-11-10 15:10:40 -0300

    visited links

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,23 +1,23 @@
 import Spinner from "~/components/Spinner";
 import Link from "next/link";
-import {formatDistance} from "date-fns";
-import {ptBR} from "date-fns/locale";
-import {useEffect, useRef} from "react";
-import {useIsIntersecting} from "~/hooks/useIsIntersecting";
-import {trpc} from "~/utils/trpc";
+import { formatDistance } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { useEffect, useRef } from "react";
+import { useIsIntersecting } from "~/hooks/useIsIntersecting";
+import { trpc } from "~/utils/trpc";
 
 interface PostListProps {
     recents?: boolean;
     userName?: string;
 }
 
-export function PostList({userName, recents}: PostListProps) {
+export function PostList({ userName, recents }: PostListProps) {
     const postScoreMap = new Map<string, number>();
 
     const [isLoadMoreVisible, ref] = useIsIntersecting<HTMLDivElement>();
 
     const query = trpc.posts.list.useInfiniteQuery(
-        {limit: 15, recents, userName},
+        { limit: 15, recents, userName },
         {
             getNextPageParam: (lastPage) => lastPage.nextCursor,
             refetchOnWindowFocus: false,
@@ -36,8 +36,7 @@ export function PostList({userName, recents}: PostListProps) {
     }, [isLoadMoreVisible, query.hasNextPage, query.isFetching]);
 
     return (
-        <div
-            className="container mx-auto flex max-w-[1012px] flex-col px-4 py-4 sm:py-6 sm:px-8">
+        <div className="container mx-auto flex max-w-[1012px] flex-col px-4 py-4 sm:py-6 sm:px-8">
             {userName ? (
                 <h1 className="mb-4 border-b border-zinc-300 pb-4 text-3xl font-bold">
                     {userName}
@@ -63,7 +62,7 @@ export function PostList({userName, recents}: PostListProps) {
                             </span>
                             <div className="ml-2">
                                 <Link
-                                    className="font-medium hover:underline"
+                                    className="font-medium visited:text-zinc-400 hover:underline"
                                     href={`/${post.author.username}/${post.slug}`}
                                 >
                                     {post.title}
@@ -91,7 +90,7 @@ export function PostList({userName, recents}: PostListProps) {
                                         {formatDistance(
                                             new Date(post.createdAt),
                                             new Date(),
-                                            {locale: ptBR}
+                                            { locale: ptBR }
                                         )}{" "}
                                         atrás
                                     </span>
@@ -103,9 +102,8 @@ export function PostList({userName, recents}: PostListProps) {
             )}
             <div ref={ref}>
                 {query.isFetchingNextPage && (
-                    <div
-                        className="flex w-full items-center justify-center p-8">
-                        <Spinner/>
+                    <div className="flex w-full items-center justify-center p-8">
+                        <Spinner />
                     </div>
                 )}
             </div>
