9e565639822214e7165f4938f7d1cf459f475dd9
greidinger.dev <jp.greidinger@hotmail.com>
2022-11-14 14:10:57 -0300

    navbar user menu

--- src/components/headlessui/Dropdown.tsx
+++ src/components/headlessui/Dropdown.tsx
@@ -1,5 +1,5 @@
-import {Menu, Transition} from "@headlessui/react";
-import {Fragment} from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 interface DropdownProps {
     button: React.ReactNode;
@@ -21,8 +21,7 @@ export default function Dropdown(props: DropdownProps) {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items
-                    className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white p-2 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-xl bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     {props.children}
                 </Menu.Items>
             </Transition>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,27 @@
-import type {AppType} from "next/app";
-import type {Session} from "next-auth";
-import {SessionProvider, signOut, useSession} from "next-auth/react";
-import {trpc} from "../utils/trpc";
+import type { AppType } from "next/app";
+import type { Session } from "next-auth";
+import { SessionProvider, signOut, useSession } from "next-auth/react";
+import { trpc } from "../utils/trpc";
 import "../styles/globals.css";
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import classNames from "classnames";
 import Dropdown from "../components/headlessui/Dropdown";
-import {FiChevronDown} from "react-icons/fi";
-import {IoNewspaperOutline} from "react-icons/io5";
-import {FaUserCircle} from "react-icons/fa";
-import {useEffect} from "react";
+import { IoNewspaperOutline } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+import { useEffect } from "react";
 import NProgress from "nprogress";
+import { AiOutlineHome } from "react-icons/ai";
 
 export const HEADER_HEIGHT = "64px";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-                                                         Component,
-                                                         pageProps: {
-                                                             session,
-                                                             ...pageProps
-                                                         },
-                                                     }) => {
+    Component,
+    pageProps: { session, ...pageProps },
+}) => {
     const router = useRouter();
 
-    NProgress.configure({showSpinner: false});
+    NProgress.configure({ showSpinner: false });
 
     useEffect(() => {
         function handleChangeStart() {
@@ -41,7 +38,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 
     return (
         <SessionProvider session={session}>
-            <Navbar/>
+            <Navbar />
             <Component {...pageProps} />
         </SessionProvider>
     );
@@ -49,81 +46,105 @@ const MyApp: AppType<{ session: Session | null }> = ({
 
 function Navbar() {
     const router = useRouter();
-    const {data: session} = useSession();
+    const { data: session } = useSession();
 
     return (
         <header
-            style={{height: HEADER_HEIGHT}}
+            style={{ height: HEADER_HEIGHT }}
             className="flex items-center justify-between gap-4 bg-zinc-800 px-4 text-white"
         >
-            <ul className="flex items-center gap-4 text-sm font-medium">
-                <li>
-                    <Link
-                        className="flex items-center gap-1 transition-all hover:text-zinc-400"
-                        href="/"
-                    >
-                        <IoNewspaperOutline className="text-2xl"/>
-                        TabNews Clone
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        className={classNames(
-                            "transition-all hover:border-zinc-400 hover:text-zinc-400",
-                            {
-                                "border-b border-white pb-1":
-                                    router.asPath === "/",
-                            }
-                        )}
-                        href="/"
-                    >
-                        Relevantes
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        className={classNames(
-                            "transition-all hover:border-zinc-400 hover:text-zinc-400",
-                            {
-                                "border-b border-white pb-1":
-                                    router.asPath === "/recentes",
-                            }
-                        )}
-                        href="/recentes"
-                    >
-                        Recentes
-                    </Link>
-                </li>
-            </ul>
+            <nav>
+                <ul className="flex items-center gap-4 text-sm font-medium">
+                    <li>
+                        <Link
+                            className="flex items-center gap-1 transition-all hover:text-zinc-400"
+                            href="/"
+                        >
+                            <IoNewspaperOutline className="text-2xl" />
+                            TabNews Clone
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className={classNames(
+                                "transition-all hover:border-zinc-400 hover:text-zinc-400",
+                                {
+                                    "border-b border-white pb-1":
+                                        router.asPath === "/",
+                                }
+                            )}
+                            href="/"
+                        >
+                            Relevantes
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className={classNames(
+                                "transition-all hover:border-zinc-400 hover:text-zinc-400",
+                                {
+                                    "border-b border-white pb-1":
+                                        router.asPath === "/recentes",
+                                }
+                            )}
+                            href="/recentes"
+                        >
+                            Recentes
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
             <div className="text-sm font-medium">
                 {session ? (
                     <div className="flex items-center gap-1">
-                        <FaUserCircle size={24}/>
-                        <Dropdown button={<FiChevronDown className="mt-1"/>}>
-                            <ul className="space-y-1 text-zinc-700">
-                                <li className="hover:text-zinc-400">
-                                    <Link href={`/${session.user?.name}`}>
+                        <Dropdown
+                            button={
+                                <div className="rounded-md bg-gray-100 p-2">
+                                    <FaUser className="text-zinc-800" />
+                                </div>
+                            }
+                        >
+                            <ul className="flex flex-col divide-y font-normal text-zinc-700">
+                                <li>
+                                    <Link
+                                        className="mx-2 my-2 flex items-center gap-2 rounded-md px-2 py-1 hover:bg-gray-100"
+                                        href={`/${session.user?.name}`}
+                                    >
+                                        <AiOutlineHome size={16} />
                                         {session.user?.name}
                                     </Link>
                                 </li>
-                                <li className="hover:text-zinc-400">
-                                    <button onClick={() => signOut()}>
-                                        Sair
-                                    </button>
-                                </li>
-                                {/* <li>
-                                    <button
-                                        onClick={() => console.log(session)}
+                                <div>
+                                    <li>
+                                        <Link
+                                            className="mx-2 mt-2 flex items-center gap-2 rounded-md px-2 py-1 hover:bg-gray-100"
+                                            href="/publicar"
+                                        >
+                                            Publicar novo conteúdo
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            className="mx-2 mb-2 flex items-center gap-2 rounded-md px-2 py-1 hover:bg-gray-100"
+                                            href="/perfil"
+                                        >
+                                            Editar perfil
+                                        </Link>
+                                    </li>
+                                </div>
+                                <li>
+                                    <span
+                                        className="my-2 mx-2 flex cursor-pointer items-center gap-2 rounded-md px-2 py-1 text-red-500 hover:bg-red-100/50"
+                                        onClick={() => signOut()}
                                     >
-                                        log
-                                    </button>
-                                </li> */}
+                                        Deslogar
+                                    </span>
+                                </li>
                             </ul>
                         </Dropdown>
                     </div>
                 ) : (
-                    <div
-                        className="flex items-center gap-2 text-sm font-medium">
+                    <div className="flex items-center gap-2 text-sm font-medium">
                         <Link
                             href="/login"
                             className="transition-all hover:text-zinc-400"
